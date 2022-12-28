41c3bcc38aec4d831e91f77012e7b3e70aa90915
Alexis Rico <sferadev@gmail.com>
2022-12-28 11:48:26 +0100

    Update bundler
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- blocks/utils/useBundler.ts
+++ blocks/utils/useBundler.ts
@@ -1,107 +1,39 @@
 import './loadProcess';
 
-import { rollup, Plugin } from '@rollup/browser';
 import { UseQueryOptions, useQuery } from '@tanstack/react-query';
+import { Plugin, build, initialize } from 'esbuild-wasm';
 import type { CallbackFsClient } from 'isomorphic-git';
-import { importCdn } from 'rollup-plugin-import-cdn';
-import { build, initialize } from 'esbuild-wasm';
-
-const SEARCH_EXTENSIONS = ['/index.tsx', '/index.ts', '/index.js', '.tsx', '.ts', '.json', '.js'];
-
-function searchFile(vfs: Map<string, string>, filepath: string, extensions: string[]) {
-  for (const ext of ['', ...extensions]) {
-    if (vfs.has(filepath + ext)) {
-      return filepath + ext;
-    }
-  }
-}
-
-const isFileSchema = (id: string) => id.startsWith('file://') || id.startsWith('/');
-
-const isRelativePath = (id: string) => stripSchema(id).startsWith('.');
-const stripSchema = (id: string) => id.replace(/^file\\:(\/\/)?/, '');
-
-const fileSystem = ({
-  fs,
-  files,
-  extensions = SEARCH_EXTENSIONS
-}: {
-  fs: CallbackFsClient;
-  files: Record<string, string>;
-  extensions?: string[];
-}) => {
-  const vfs = new Map(Object.entries(files));
-
-  return {
-    name: 'virtual-fs',
-    resolveId(id: string, importer: string | null) {
-      const normalized = stripSchema(id);
-
-      // entry point
-      if (isFileSchema(id) && importer == null) {
-        return searchFile(vfs, normalized, extensions);
-      }
-
-      // relative filepath
-      if (importer && isFileSchema(importer) && isRelativePath(id)) {
-        const fullpath = id;
-        const reslovedWithExt = searchFile(vfs, fullpath, extensions);
-        if (reslovedWithExt) return reslovedWithExt;
-        console.warn('[rollup-plugin-virtual-fs] can not resolve id: ' + fullpath);
-      }
-    },
-    load(id: string) {
-      const real = stripSchema(id);
-      const ret = vfs.get(real);
-      if (ret) return ret;
-
-      // load file from fs
-      const file = fs.readFile(real);
-      if (file) return file;
-    }
-  };
-};
 
 type UseBundlerOptions = {
   fs: CallbackFsClient;
   code: string;
 };
 
-let myPlugin = {
-  name: 'example',
-  setup(build: any) {
-    build.onResolve({ filter: /.*/ }, (args: any) => {
-      // here i just replace the prefix './' with '/',
-      // but normally you have to implement the resolver correctly
-      return { path: '/' + args.path.replace(/^\.\//, '') };
+const virtualFs = ({ fs, files = {} }: { fs: CallbackFsClient; files?: Record<string, string> }): Plugin => ({
+  name: 'virtual-fs',
+  setup: (build) => {
+    const vfs = new Map(Object.entries(files));
+
+    // TODO: Add proper resolve logic
+    build.onResolve({ filter: /.*/ }, (args) => {
+      return { path: args.path };
     });
-    build.onLoad({ filter: /.*/ }, (args: any) => {
-      return { contents: 'console.log("M")', loader: 'default' };
+
+    // TODO: Add CDN loading, fully test relative imports in fs
+    build.onLoad({ filter: /.*/ }, (args) => {
+      const file = vfs.get(args.path) ?? fs.readFile(args.path);
+      if (file) return { contents: file, loader: 'default' };
     });
   }
-};
+});
 
+const entryPoint = '/__entry.ts';
 let initialized = false;
 
 export function useBundler({ fs, code }: UseBundlerOptions, queryOptions?: UseQueryOptions) {
   return useQuery(
     ['bundle', code],
     async () => {
-      const input = 'index.ts';
-
-      /**const bundle = await rollup({
-        input,
-        plugins: [
-          importCdn({ fetchImpl: fetch }),
-          fileSystem({ fs, files: { [input]: code } }),
-          typescript()
-        ] as Plugin[]
-      });
-
-      const { output } = await bundle.generate({ format: 'esm' });
-
-      return output;**/
-
       if (!initialized) {
         initialized = true;
         await initialize({
@@ -111,10 +43,10 @@ export function useBundler({ fs, code }: UseBundlerOptions, queryOptions?: UseQu
       }
 
       const result = await build({
-        entryPoints: [input],
+        entryPoints: [entryPoint],
         bundle: true,
         write: false,
-        plugins: [myPlugin]
+        plugins: [virtualFs({ fs, files: { [entryPoint]: code } })]
       });
 
       return result.outputFiles[0].text;
